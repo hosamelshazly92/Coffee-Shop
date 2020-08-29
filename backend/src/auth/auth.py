81ccd4e81a1a7af0a8f30a2821d270d15e4ef012
hosam <hosamelshazly92@gmail.com>
2020-08-29 15:36:45 +0200

    check PEP8 requirements for auth.py

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -8,17 +8,19 @@ AUTH0_DOMAIN = 'dev-1fg43j6s.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee'
 
-## AuthError Exception
+# AuthError Exception
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
-## Auth Header
+# Auth Header
 
 '''
 @TODO_DONE implement get_token_auth_header() method
@@ -28,6 +30,8 @@ class AuthError(Exception):
         it should raise an AuthError if the header is malformed
     return the token part of the header
 '''
+
+
 def get_token_auth_header():
 
     if 'Authorization' not in request.headers:
@@ -51,9 +55,11 @@ def get_token_auth_header():
 
     it should raise an AuthError if permissions are not included in the payload
         !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
+    it should raise an AuthError if the requested permission string is not in
+    the payload permissions array return true otherwise
 '''
+
+
 def check_permissions(permissions, payload):
     if 'permissions' not in payload:
         abort(400)
@@ -75,8 +81,11 @@ def check_permissions(permissions, payload):
     it should validate the claims
     return the decoded payload
 
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+    !!NOTE urlopen has a common certificate error described here:
+    https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-
+    verify-failed-error-for-http-en-wikipedia-org
 '''
+
 
 def verify_decode_jwt(token):
 
@@ -123,7 +132,8 @@ def verify_decode_jwt(token):
         except jwt.JWTClaimsError:
             raise AuthError({
                 'code': 'invalid_claims',
-                'description': 'Incorrect claims. Please, check the audience and issuer.'
+                'description':
+                    'Incorrect claims. Please, check the audience and issuer.'
             }, 401)
         except Exception:
             raise AuthError({
@@ -142,9 +152,11 @@ def verify_decode_jwt(token):
 
     it should use the get_token_auth_header method to get the token
     it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    it should use the check_permissions method validate claims and check
+    the requested permission return the decorator which passes the decoded
+    payload to the decorated method
 '''
+
 
 def requires_auth(permissions=[]):
     def requires_auth_decorator(f):
